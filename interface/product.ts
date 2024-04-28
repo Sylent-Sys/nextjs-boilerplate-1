ad87de00e32403cfc3443585de5344af7042822a
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-04-28 10:30:46 +0700

    Add product interface and types

---
+++ interface/product.ts
@@ -0,0 +1,16 @@
+export type IProducts = IProduct[]
+
+export interface IProduct {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+    rating: IRating
+}
+
+export interface IRating {
+    rate: number
+    count: number
+}
